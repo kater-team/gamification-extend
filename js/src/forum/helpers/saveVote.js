b2f3b19a89f08e3d0f51d1348852dc4d0893a797
Hehongyuanlove <1016823618@qq.com>
2022-08-04 05:33:51 +0000

    add(Post vote)

---
+++ js/src/forum/helpers/saveVote.js
@@ -0,0 +1,39 @@
+import app from 'flarum/forum/app';
+import DiscussionControls from 'flarum/forum/utils/DiscussionControls';
+
+export default (post, upvoted, downvoted, load, discussion = post.discussion()) => {
+  if (!app.session.user) {
+    // We use this instead of showing LogInModal so that extensions can override it
+    DiscussionControls.replyAction.call(discussion, true);
+    return;
+  } else if (discussion && !discussion.canVote() && !post.canVote()) {
+    return;
+  }
+
+  if (upvoted && downvoted) {
+    upvoted = false;
+    downvoted = false;
+  }
+
+  if (load) load(true);
+
+  m.redraw();
+
+  return post
+    .save([upvoted, downvoted, 'vote'])
+    .then(
+      () => null,
+      () => null
+    )
+    .then(() => {
+      if (load) load(false);
+
+      if (discussion) {
+        discussion.pushAttributes({
+          votes: post.votes(),
+        });
+      }
+
+      m.redraw();
+    });
+};
